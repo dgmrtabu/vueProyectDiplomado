5ecd2bc6e885a4cd75986edb0e4f59be3760eb28
david.montano <david.montano@cns.gob.bo>
2026-01-22 16:32:10 -0400

    Proyecto Vue aplicacion de Login aplicacion DahboardLayout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import AuthLayouts from '@/layouts/AuthLayouts.vue'
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
@@ -12,6 +13,23 @@ const routes: RouteRecordRaw[] = [
     children: [
       { path: 'login', name: 'login', component: LoginView },
       { path: 'register', name: 'register', component: RegisterView },
+    ],
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      // {
+      //   path: '/profile',
+      //   name: 'profile',
+      //   component: PerfilView,
+      // },
+      // {
+      //   path: '/tasks',
+      //   name: 'tasks',
+      //   component: () => import('@/views/dashboard/TasksView.vue'),
+      // },
     ],
   },
 ]
